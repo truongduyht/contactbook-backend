2181583ddac77e56ce098cef2ef493c2e17d78b7
B2014734 <duyb2014734@student.ctu.edu.vn>
2024-02-23 16:49:17 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors()); 
 app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
 
