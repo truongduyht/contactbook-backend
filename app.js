478de3ae8ba45b670ba60149ce8da52ee5405cfd
B2014734 <duyb2014734@student.ctu.edu.vn>
2024-03-08 14:39:04 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const contactsRouter = require("./app/routes/contact.route");
 app.use(cors()); 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
 app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
-});
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
 });
 
 module.exports = app;
